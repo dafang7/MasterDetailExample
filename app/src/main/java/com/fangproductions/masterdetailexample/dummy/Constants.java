e864410c73dbe0f0f9c047becdbd4d3d7fc559e9
Derek F <dafang7@gmail.com>
2015-02-24 13:56:07 -0600

    Added the constants class

---
+++ app/src/main/java/com/fangproductions/masterdetailexample/dummy/Constants.java
@@ -0,0 +1,10 @@
+package com.fangproductions.masterdetailexample.dummy;
+
+/**
+ * Created by derek.fang on 2/24/2015.
+ */
+public class Constants {
+
+   public static final String KEY = "key";
+   public static final String KEY2 = "Key 2";
+}
